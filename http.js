3ab673e6a38907b68016bd8b5ca887428b61918b
nurhanteymurkhanli <94322847+nurhanteymurkhanli@users.noreply.github.com>
2025-01-11 23:44:42 +0400

    Update http.js

--- http.js
+++ http.js
@@ -1,1 +1,12 @@
 module.exports = require("./").http;
+// Eski kod (Node.js native http modülü kullanılarak)
+const http = require('http');
+http.get('http://example.com', (response) => {
+    console.log(response);
+});
+
+// Yeni kod (follow-redirects kullanılarak)
+const { http } = require('follow-redirects');
+http.get('http://example.com', (response) => {
+    console.log(response.responseUrl); // Yönlendirme sonucu URL
+});
